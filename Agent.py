3d993ef65a440f4818ebcc4fa068079e6656dd30
S zy <88704291+dashuai77@users.noreply.github.com>
2021-08-11 12:11:05 +0800

    new

--- Agent.py
+++ Agent.py
@@ -2,21 +2,7 @@
     def __init__(self, gamma, epsilon, lr, n_actions, input_dims,
                  mem_size, batch_size, eps_min=0.01, eps_dec=5e-7,
                  replace=1000, algo=None, env_name=None, chkpt_dir='tmp/dqn'):
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.lr = lr
-        self.n_actions = n_actions
-        self.input_dims = input_dims
-        self.batch_size = batch_size
-        self.eps_min = eps_min
-        self.eps_dec = eps_dec
-        self.replace_target_cnt = replace
-        self.algo = algo
-        self.env_name = env_name
-        self.chkpt_dir = chkpt_dir
-        self.action_space = [i for i in range(n_actions)]
-        self.learn_step_counter = 0
-
+    
         self.memory = ReplayBuffer(mem_size, input_dims, n_actions)
 
         self.q_eval = DeepQNetwork(self.lr, self.n_actions,
